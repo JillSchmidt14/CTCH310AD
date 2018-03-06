e46a01a5c188ba35f6d05910731f690763ffaec9
JillSchmidt14 <schmijil@udml13.cs.uregina.ca>
2018-03-06 16:27:57 -0600

    Beginning Hello script

---
+++ Hello.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Hello Earth");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//print ("Hello Earth");
+
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (0f, 0f, 0.1f);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (-0.1f, 0f, 0f);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (0.1f, 0f, 0f);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (0f, 0f, -0.1f);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate (0f, 1f, 0f);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate (0f, -1f, 0f);
+		}
+	}
+}
