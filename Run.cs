505feeab8cb271548559c47a8a6728d23ddfe7c0
JillSchmidt14 <37124570+JillSchmidt14@users.noreply.github.com>
2018-03-06 16:38:36 -0600

    Add files via upload
    Up to Lab 4 Code files

---
+++ Run.cs
@@ -0,0 +1,38 @@
+﻿//*Run
+//Created By: Jill Schmidt
+//Feb.14th/18
+//CTCH310
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run : MonoBehaviour {
+
+	public float speed;
+	public GameObject  player;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		float dist = Vector3.Distance (player.transform.position, transform.position);
+
+		Vector3 localPosition = player.transform.position - transform.position;
+		localPosition = localPosition.normalized;
+
+		if (dist < 5) {
+			transform.Translate (localPosition.x * Time.deltaTime * speed * -1,
+				0.0f,
+				localPosition.z * Time.deltaTime * speed * -1);
+		}
+
+
+	}
+}
